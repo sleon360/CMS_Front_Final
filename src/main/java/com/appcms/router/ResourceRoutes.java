f2c2cbb9bd01b8de917912a1cf19486be28dcdf7
user <erodriguez@lealtad360.com>
2019-07-20 16:59:27 -0400

    Se modifica la consulta al servicio que carga los recursos para adpatarse al servicio del bff modificado

--- src/main/java/com/appcms/router/ResourceRoutes.java
+++ src/main/java/com/appcms/router/ResourceRoutes.java
@@ -8,17 +8,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.appcms.entity.ResourceEntity;
 
@@ -33,19 +33,20 @@ public class ResourceRoutes {
 	}
 
 	@GetMapping("/resource/{folder}/{resourceid:.+}")
-	public Object resource(@PathVariable("resourceid") String resourceid, @PathVariable("folder") String folder,
+	public Object resource(@PathVariable("resourceid") String resourceName, @PathVariable("folder") String folder,
 			HttpServletRequest request, HttpServletResponse response) {
 		try {
 
 			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 			headers.set("Authorization", request.getSession().getAttribute("TOKENONE").toString());
-			MultiValueMap<String, String> xparam = new LinkedMultiValueMap<String, String>();
-			xparam.add("idresource", resourceid);
-			HttpEntity<?> httpEntity = new HttpEntity<Object>(xparam, headers);
+
+			UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl + "/resource/" + folder + "/get")
+			        .queryParam("name", resourceName);
+			
+			HttpEntity<?> httpEntity = new HttpEntity<Object>(headers);
 			RestTemplate restTemplate = new RestTemplate();
 			ResponseEntity<ResourceEntity> xresponse = restTemplate
-					.postForEntity(apiUrl + "/resource/" + folder + "/get", httpEntity, ResourceEntity.class);
+					.exchange(builder.toUriString(), HttpMethod.GET, httpEntity, ResourceEntity.class);
 
 			if (xresponse.getStatusCode() == HttpStatus.OK) {
 				ResourceEntity rEntity = xresponse.getBody();
