c5c0b66665c80a101b1b964bbfa6c5f3fcebd6c6
user <erodriguez@lealtad360.com>
2019-12-05 09:34:54 -0300

    Corrección de bug de despegar

--- src/main/java/com/appcms/router/RouteDespegar.java
+++ src/main/java/com/appcms/router/RouteDespegar.java
@@ -5,12 +5,14 @@ import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.appcms.services.CustomerService;
 
+@Controller
 public class RouteDespegar {
 
 	@Autowired
